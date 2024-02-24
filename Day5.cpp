8acfed24d467fa2d084754e73a3a3f33bbb8cb0b
bhoomikamra <78876138+bhoomikamra@users.noreply.github.com>
2024-02-24 13:32:46 +0530

    Create Day5.cpp
    Array Intersection:
    Find the intersection of two arrays.

---
+++ Day5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+void findIntersection(int arr1[], int m, int arr2[], int n) {
+    // Your code here
+}
+
+int main() {
+    int arr1[] = {1, 2, 2, 1};
+    int m = sizeof(arr1) / sizeof(arr1[0]);
+    int arr2[] = {2, 2};
+    int n = sizeof(arr2) / sizeof(arr2[0]);
+    // Display the intersection
+    return 0;
+}

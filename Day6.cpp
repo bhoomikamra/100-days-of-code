80633efda09d0cea51af23cbebfd1a548742cab9
bhoomikamra <78876138+bhoomikamra@users.noreply.github.com>
2024-02-25 23:25:09 +0530

    Create Day6.cpp
    Product of Array Except Self:
    Given an array nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].

---
+++ Day6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> productExceptSelf(vector<int>& nums) {
+    // Your code here
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> result = productExceptSelf(nums);
+    // Display the result vector
+    return 0;
+}

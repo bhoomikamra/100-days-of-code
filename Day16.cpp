edbc6539281e3385b39fe7d5a22e0f46bd7abea5
bhoomikamra <78876138+bhoomikamra@users.noreply.github.com>
2024-03-05 11:01:10 +0530

    Create Day16.cpp
    Reverse Linked List

---
+++ Day16.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* current = head;
+    ListNode* nextNode = nullptr;
+
+    while (current != nullptr) {
+        nextNode = current->next;
+        current->next = prev;
+        prev = current;
+        current = nextNode;
+    }
+
+    return prev;
+}
+
+int main() {
+    // Test your linked list reversal function
+    return 0;
+}

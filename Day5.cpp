d092e7eaffe11c4a8b215b5bd2cf60fdfe352304
bhoomikamra <78876138+bhoomikamra@users.noreply.github.com>
2024-03-09 19:07:50 +0530

    Day5.cpp
    Array Intersection

--- Day5.cpp
+++ Day5.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 void findIntersection(int arr1[], int m, int arr2[], int n) {
-    // Your code here
+    unordered_set<int> set1(arr1, arr1 + m);
+    unordered_set<int> result;
+
+    for (int i = 0; i < n; ++i) {
+        if (set1.count(arr2[i])) {
+            result.insert(arr2[i]);
+        }
+    }
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
 }
 
 int main() {
@@ -11,6 +22,6 @@ int main() {
     int m = sizeof(arr1) / sizeof(arr1[0]);
     int arr2[] = {2, 2};
     int n = sizeof(arr2) / sizeof(arr2[0]);
-    // Display the intersection
+    findIntersection(arr1, m, arr2, n);
     return 0;
 }

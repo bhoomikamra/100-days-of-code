2d0e73bf5ecb3081ed75bdb82f5dc0d311886056
bhoomikamra <78876138+bhoomikamra@users.noreply.github.com>
2024-03-09 19:04:53 +0530

    Day2.cpp
    FIND MISSING NUMBER

--- Day2.cpp
+++ Day2.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int findMissingNumber(int arr[], int n) {
-    // Your code here
+    int expectedSum = n * (n + 1) / 2;
+    int actualSum = 0;
+
+    for (int i = 0; i < n; ++i) {
+        actualSum += arr[i];
+    }
+
+    return expectedSum - actualSum;
 }
 
 int main() {
